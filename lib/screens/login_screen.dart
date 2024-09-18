2f15a0e0a6d5e706c74038487c81eaad089927f8
leo8 <leoleo8@hotmail.fr>
2024-09-18 04:24:05 +0200

    Implementing login and signup screens

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,113 @@
+import 'package:cfq_dev/utils/colors.dart';
+import 'package:cfq_dev/widgets/text_field_input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            children: [
+              // Flexible(child: Container(), flex: 2),
+              Image.asset('assets/logo_white.png'), // png plus lourds ?
+
+              // SvgPicture.asset(
+              //   'assets/logo_white.svg',
+              //   color: primaryColor,
+              //   height: 64,
+              // ),
+
+              const SizedBox(
+                height: 64,
+              ),
+              // Email input
+              TextFieldInput(
+                hintText: "Ton email",
+                textInputType: TextInputType.emailAddress,
+                textEditingController: _emailController,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              // Password input
+              TextFieldInput(
+                hintText: "Ton mot de passe",
+                textInputType: TextInputType.text,
+                textEditingController: _passwordController,
+                isPassword: true,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              // Log in button
+              InkWell(
+                child: Container(
+                  child: const Text('Log in'),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(4),
+                      ),
+                    ),
+                    color: blueColor,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Flexible(child: Container(), flex: 2),
+              // Transition to sign up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text('Pas encore de compte ?'),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      child: Text(
+                        "Je m'inscris",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,139 @@
+import 'package:cfq_dev/utils/colors.dart';
+import 'package:cfq_dev/widgets/text_field_input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameController.dispose();
+    _bioController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            children: [
+              // Flexible(child: Container(), flex: 2),
+              Image.asset('assets/logo_white.png'), // png plus lourds (cf login_screen.dart) ?
+              const SizedBox(
+                height: 32,
+              ),
+              Stack(
+                children: [
+                  CircleAvatar(
+                    radius: 64,
+                    backgroundImage: NetworkImage('https://as1.ftcdn.net/v2/jpg/05/16/27/58/1000_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg'),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              // Email input
+              TextFieldInput(
+                hintText: "Ton email",
+                textInputType: TextInputType.emailAddress,
+                textEditingController: _emailController,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              // Password input
+              TextFieldInput(
+                hintText: "Ton mot de passe",
+                textInputType: TextInputType.text,
+                textEditingController: _passwordController,
+                isPassword: true,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              // Password input
+              TextFieldInput(
+                hintText: "Un petit nom ?",
+                textInputType: TextInputType.text,
+                textEditingController: _usernameController,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              // Password input
+              TextFieldInput(
+                hintText: "Ta bio",
+                textInputType: TextInputType.text,
+                textEditingController: _bioController,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              // Sign up button
+              InkWell(
+                child: Container(
+                  child: const Text('Inscription'),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(4),
+                      ),
+                    ),
+                    color: blueColor,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Flexible(child: Container(), flex: 2),
+              // Transition to log in
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text('Déjà inscrit.e ?'),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      child: Text(
+                        "Je me connecte",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
