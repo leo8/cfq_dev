6915128ed812b0fd19079d7d79e514006601160a
leo8 <leoleo8@hotmail.fr>
2024-09-19 22:57:56 +0200

    Fix on profile picture upload

--- lib/ressources/auth_methods.dart
+++ lib/ressources/auth_methods.dart
@@ -2,14 +2,13 @@ import 'dart:typed_data';
 import 'package:cfq_dev/ressources/storage_methods.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:cfq_dev/models/user.dart' as model;
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  // Fetching user details
   Future<model.User> getUserDetails() async {
     User currentUser = _auth.currentUser!;
 
@@ -19,38 +18,47 @@ class AuthMethods {
     return model.User.fromSnap(snap);
   }
 
-  Future<String> signUpUser(
-      {required String email,
-      required String password,
-      required String username,
-      String? location,
-      String? bio,
-      Uint8List? profilePicture}) async {
+  //Sign up user
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    String? location,
+    String? bio,
+    Uint8List? profilePicture,
+  }) async {
     String res = 'Some error occurred';
     try {
-      if (email.isNotEmpty || password.isNotEmpty || username.isNotEmpty) {
-        // Register user
+      if (email.isNotEmpty && password.isNotEmpty && username.isNotEmpty) {
+        // Register the user
         UserCredential userCredential = await _auth
             .createUserWithEmailAndPassword(email: email, password: password);
 
         // Store profilePicture and get profilePictureUrl
         String profilePictureUrl = '';
         if (profilePicture != null) {
-          String profilePictureUrl = await StorageMethods()
+          profilePictureUrl = await StorageMethods()
               .uploadImageToStorage('profilePicture', profilePicture, false);
+
+          // Log the URL to ensure it's not empty
+          print('Profile picture URL: $profilePictureUrl');
         }
 
-        // Create user with data model
+        // If profilePictureUrl is still empty, something went wrong
+        if (profilePictureUrl.isEmpty) {
+          return 'Failed to upload profile picture';
+        }
 
+        // Create user data with the provided model
         model.User user = model.User(
           username: username,
           uid: userCredential.user!.uid,
-          bio: (bio != null) ? bio : "",
+          bio: bio ?? "",
           email: email,
           followers: [],
           following: [],
-          profilePictureUrl: profilePictureUrl,
-          location: (location != null) ? location : "",
+          profilePictureUrl: profilePictureUrl, // Correct assignment
+          location: location ?? "",
           isActive: false,
         );
 
@@ -58,7 +66,10 @@ class AuthMethods {
         await _firestore.collection('users').doc(userCredential.user!.uid).set(
               user.toJson(),
             );
+
         res = 'success';
+      } else {
+        res = 'Please fill in all required fields';
       }
     } catch (err) {
       res = err.toString();
@@ -67,17 +78,20 @@ class AuthMethods {
     return res;
   }
 
-  Future<String> logInUser(
-      {required String email, required String password}) async {
+  // Log in method
+  Future<String> logInUser({
+    required String email,
+    required String password,
+  }) async {
     String res = 'Some error occurred';
 
     try {
-      if (email.isNotEmpty || password.isNotEmpty) {
+      if (email.isNotEmpty && password.isNotEmpty) {
         await _auth.signInWithEmailAndPassword(
             email: email, password: password);
         res = 'success';
       } else {
-        res = "Certains champs sont vides";
+        res = "Please fill in all fields";
       }
     } catch (err) {
       res = err.toString();
@@ -85,6 +99,7 @@ class AuthMethods {
     return res;
   }
 
+  // Log out method
   Future<String> logOutUser() async {
     String res = 'Some error occurred';
 
