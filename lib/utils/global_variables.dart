ad7eea6196ba5a13f40e4439a419b4e93a59cb4c
leo8 <leoleo8@hotmail.fr>
2024-09-19 18:27:58 +0200

    Implementing add_post_screen

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,4 +1,5 @@
 import 'package:cfq_dev/screens/add_cfq_screen.dart';
+import 'package:cfq_dev/screens/add_post_screen.dart';
 import 'package:cfq_dev/screens/add_turn_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +7,7 @@ const webScreenSize = 600;
 const homeScreenItems = [
   Center(child: Text('Feed')),
   Center(child: Text('Map')),
-  //AddCfqScreen(),
-  AddTurnScreen(),
+  AddPostScreen(),
   Center(child: Text('Calendar')),
   Center(child: Text('Profile')),
 ];
