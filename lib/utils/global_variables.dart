00dec993096dc39d521abadac8fdd1625b8630d4
leo8 <leoleo8@hotmail.fr>
2024-09-19 23:48:21 +0200

    Updating global list of screens

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -2,11 +2,12 @@ import 'package:cfq_dev/screens/add_cfq_screen.dart';
 import 'package:cfq_dev/screens/add_post_screen.dart';
 import 'package:cfq_dev/screens/add_turn_screen.dart';
 import 'package:cfq_dev/screens/profile_screen.dart';
+import 'package:cfq_dev/screens/thread_screen.dart';
 import 'package:flutter/material.dart';
 
 const webScreenSize = 600;
 const homeScreenItems = [
-  Center(child: Text('Feed')),
+  ThreadScreen(),
   Center(child: Text('Map')),
   AddPostScreen(),
   Center(child: Text('Calendar')),
