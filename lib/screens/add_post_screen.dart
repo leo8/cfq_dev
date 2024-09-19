62c23b358fec2452872209d74c267623c6881f64
leo8 <leoleo8@hotmail.fr>
2024-09-19 02:18:48 +0200

    Implemeting add post screen

---
+++ lib/screens/add_post_screen.dart
@@ -0,0 +1,44 @@
+import 'package:cfq_dev/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back,
+          ),
+        ),
+        title: const Text('Turn'),
+        centerTitle: false,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Post',
+              style: TextStyle(
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Column(children: [
+        Row(children: [],)
+      ],)
+    );
+  }
+}
