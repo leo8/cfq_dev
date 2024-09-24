437c12fd0694a455bae2811f10f47044fbc8815c
leo8 <leoleo8@hotmail.fr>
2024-09-24 12:36:09 +0200

    Replacing hardcoded dimensions with CustomDimension variables

--- lib/responsive/repsonsive_layout_screen.dart
+++ lib/responsive/repsonsive_layout_screen.dart
@@ -1,5 +1,5 @@
 import 'package:cfq_dev/providers/user_provider.dart';
-import 'package:cfq_dev/utils/global_variables.dart';
+import 'package:cfq_dev/utils/dimensions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -32,7 +32,7 @@ class _RepsonsiveLayoutState extends State<RepsonsiveLayout> {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        if (constraints.maxWidth > webScreenSize) {
+        if (constraints.maxWidth > CustomDimension.webScreenSize) {
           return widget.webScreenLayout;
         }
         return widget.mobileScreenLayout;
