6be99b54b8129f8c67e9f7deacbfbf34b3740b80
leo8 <leoleo8@hotmail.fr>
2024-09-25 08:58:04 +0200

    Implementing atomic design for add_turn_screen

---
+++ lib/molecules/address_fields_row.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:cfq_dev/atoms/texts/custom_text_field.dart';
+import 'package:cfq_dev/utils/string.dart';
+
+class AddressFieldsRow extends StatelessWidget {
+  final TextEditingController locationController;
+  final TextEditingController addressController;
+
+  const AddressFieldsRow({
+    required this.locationController,
+    required this.addressController,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: CustomTextField(
+            controller: locationController,
+            hintText: CustomString.ou,
+          ),
+        ),
+        const SizedBox(width: 8),
+        Expanded(
+          child: CustomTextField(
+            controller: addressController,
+            hintText: CustomString.adresse,
+          ),
+        ),
+      ],
+    );
+  }
+}
