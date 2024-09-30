43271f495cbcd309408160098a4554422753ad79
CalvignacNeoxia <charles.calvignac@neoxia.com>
2024-09-30 17:38:53 +0200

    Create new nav bar

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,3 +1,6 @@
+import 'package:cfq_dev/screens/add_post_screen.dart';
+import 'package:cfq_dev/screens/profile_screen.dart';
+import 'package:cfq_dev/screens/thread_screen.dart';
 import 'package:cfq_dev/utils/home_screen_items.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,101 +16,192 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
-  int _page = 0;
-  late PageController pageController;
+  int currentPageIndex = 0;
+  bool isClicked = false;
+  bool isOpen = false;
+  double _width = 45.0;
+  final double _height = 45.0;
+  double _yPositionPlusButtonClose = 0.95;
+  double _yPositionPlusButtonOpen = 0.80;
+  double _yPositionPlusButton = 0.95;
+  bool _showButtons = false;
 
-  @override
-  void initState() {
-    super.initState();
-    pageController = PageController(initialPage: 0);
-  }
+  void _handleTap() {
+    if (isClicked) {
+      return;
+    }
+    isClicked = true;
 
-  @override
-  void dispose() {
-    super.dispose();
-    pageController.dispose();
-  }
+    if (isOpen) {
+      // Fermer horizontalement puis descendre
+      setState(() {
+        _width = 45; // Fermer horizontalement d'abord
+      });
+      Future.delayed(const Duration(milliseconds: 200), () {
+        setState(() {
+          _yPositionPlusButton = _yPositionPlusButtonClose; // Descendre ensuite
+          isClicked = false;
+        });
+      });
+      _showButtons = false;
+    } else {
+      // Monter d'abord, puis ouvrir horizontalement
+      setState(() {
+        _yPositionPlusButton = _yPositionPlusButtonOpen; // Monter d'abord
+      });
+      Future.delayed(const Duration(milliseconds: 200), () {
+        setState(() {
+          _width = 200; // Ouvrir horizontalement ensuite
+        });
+      });
+      Future.delayed(const Duration(milliseconds: 500), () {
+        setState(() {
+          _showButtons = true; // Afficher les boutons
+          isClicked = false;
+        });
+      });
+    }
 
-  void navigationTapped(int page) {
-    pageController.jumpToPage(page);
-  }
-
-  void onPageChanged(int page) {
-    setState(() {
-      _page = page;
-    });
+    // Inverser l'état après les actions
+    isOpen = !isOpen;
   }
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
     return Scaffold(
-      body: PageView(
-        physics: const NeverScrollableScrollPhysics(),
-        controller: pageController,
-        onPageChanged: onPageChanged,
-        children: CustomHomeScreenItems.homeScreenItems,
-      ),
-      bottomNavigationBar: CupertinoTabBar(
-        iconSize: 32,
-        items: [
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: const EdgeInsets.only(top: 13),
-              child: Icon(
-                CustomIcon.languageOutlined,
-                color: _page == 0
-                    ? CustomColor.primaryColor
-                    : CustomColor.secondaryColor,
+      extendBody: true,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Stack(
+        children: [
+          // Positioned element for the button with transformation-like behavior
+          AnimatedAlign(
+            alignment: Alignment(0, _yPositionPlusButton),
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeInOut,
+            child: GestureDetector(
+              onTap: _handleTap,
+              child: AnimatedContainer(
+                width: _width,
+                height: _height,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(isOpen ? 10.0 : 0.0),
+                ),
+                // Define how long the animation should take.
+                duration: const Duration(milliseconds: 500),
+                // Provide an optional curve to make the animation feel smoother.
+                curve: Curves.easeInOut,
+                child: Transform.rotate(
+                  alignment: Alignment.center,
+                  angle: isOpen ? 0.75 : 0,
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                ),
               ),
             ),
           ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: const EdgeInsets.only(top: 13),
-              child: Icon(
-                CustomIcon.locationOnOutlined,
-                color: _page == 1
-                    ? CustomColor.primaryColor
-                    : CustomColor.secondaryColor,
-              ),
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: const EdgeInsets.only(top: 13),
-              child: Icon(
-                CustomIcon.addCircleOutlineRounded,
-                color: _page == 2
-                    ? CustomColor.purpleAccent
-                    : CustomColor.secondaryColor,
-              ),
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: const EdgeInsets.only(top: 13),
-              child: Icon(
-                CustomIcon.calendarTodayOutlined,
-                color: _page == 3
-                    ? CustomColor.primaryColor
-                    : CustomColor.secondaryColor,
-              ),
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: const EdgeInsets.only(top: 13),
-              child: Icon(
-                CustomIcon.personOutlined,
-                color: _page == 4
-                    ? CustomColor.primaryColor
-                    : CustomColor.secondaryColor,
-              ),
+          Align(
+            alignment: Alignment(0, _yPositionPlusButton),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (isOpen)
+                  AnimatedOpacity(
+                    duration: const Duration(milliseconds: 200),
+                    curve: Curves.fastOutSlowIn,
+                    opacity: _showButtons ? 1 : 0,
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      ),
+                      splashColor: Colors.black.withAlpha(0),
+                      onPressed: () {
+                        print("click edit");
+                      },
+                    ),
+                  ),
+                const SizedBox(width: 90),
+                if (isOpen)
+                  AnimatedOpacity(
+                    duration: const Duration(milliseconds: 200),
+                    curve: Curves.fastOutSlowIn,
+                    opacity: _showButtons ? 1 : 0,
+                    child: IconButton(
+                      splashColor: Colors.black.withAlpha(0),
+                      focusColor: Colors.black.withAlpha(0),
+                      icon: const Icon(
+                        Icons.camera_alt,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        print("click photo");
+                      },
+                    ),
+                  ),
+              ],
             ),
           ),
         ],
-        onTap: navigationTapped,
       ),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: const Icon(
+              CustomIcon.languageOutlined,
+              color: Colors.black,
+            ),
+            label: '',
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 40.0),
+            child: NavigationDestination(
+              icon: const Icon(
+                CustomIcon.locationOnOutlined,
+                color: Colors.black,
+              ),
+              label: '',
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 40.0),
+            child: NavigationDestination(
+              icon: const Icon(
+                CustomIcon.calendarTodayOutlined,
+                color: Colors.black,
+              ),
+              label: '',
+            ),
+          ),
+          NavigationDestination(
+            icon: const Icon(
+              CustomIcon.personOutlined,
+              color: Colors.black,
+            ),
+            label: '',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        /// Home page
+        ThreadScreen(),
+        Center(child: Text('Map')),
+        AddPostScreen(),
+        Center(child: Text('Calendar')),
+        ProfileScreen()
+      ][currentPageIndex],
     );
   }
 }
