e820a3b811013891c899740bc431c9599ce02bd0
CalvignacNeoxia <charles.calvignac@neoxia.com>
2024-10-04 22:35:18 +0100

    add secrets

---
+++ lib/secrets/secrets_firebase.dart
@@ -0,0 +1,5 @@
+var apiKey = "AIzaSyBcr5WVuZxga_98ZFohPuJ4assvOdDXjC4"; // Your API key
+var appId = "1:160341522687:web:10b0092c6c1c7ba9e574ca"; // Your app ID
+var messagingSenderId = "160341522687"; // Sender ID
+var projectId = "cfq-dev-11498"; // Project ID
+var storageBucket = "cfq-dev-11498.appspot.com";
