6561247e9af2af0c618044e8b357ff7ce582927a
leo8 <leoleo8@hotmail.fr>
2024-10-11 14:11:31 +0200

    Reorganizing custom colors to make it clear and easily editable

--- lib/utils/styles/colors.dart
+++ lib/utils/styles/colors.dart
@@ -2,22 +2,38 @@ import 'package:flutter/material.dart';
 
 // Define custom colors
 class CustomColor {
+  //CFQ Theme
   static const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
   static const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
   static const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
+  static const secondaryColor = Colors.grey;
+  static const turnColor = Colors.cyanAccent;
+  static const offColor = Colors.purpleAccent;
+
+  // Standard colors
+  static const transparent = Colors.transparent;
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const grey = Colors.grey;
+  static const green = Colors.green;
+  static const red = Colors.red;
+  static const blue = Colors.blue;
+  static const purple = Colors.purple;
+
+  // Variant
+
   static const white24 = Colors.white24;
   static const white54 = Colors.white54;
   static const white70 = Colors.white70;
-  static const white = Colors.white;
-  static const secondaryColor = Colors.grey;
-  static const purple = Colors.purple;
+
+  static const grey300 = Color(0xFFE0E0E0);
+  static const grey900 = Color(0xFF212121);
+
+  static const blueAccent = Colors.blueAccent;
+
   static const purpleAccent = Colors.purpleAccent;
   static const deepPurpleAccent = Colors.deepPurpleAccent;
   static const personnalizedPurple = Color(0xFF7A00FF);
-  static const blueColor = Color.fromRGBO(0, 149, 246, 1);
-  static const blueAccent = Colors.blueAccent;
-  static const greenColor = Color.fromARGB(255, 19, 142, 8);
+
   static const pinkAccent = Colors.pinkAccent;
-  static const black = Colors.black;
-  static const transparent = Colors.transparent;
 }
