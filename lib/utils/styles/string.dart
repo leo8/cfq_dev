0e61c65a09036ee70ef467cc66a76d4a48917632
leo8 <leoleo8@hotmail.fr>
2024-10-11 12:41:48 +0200

    Translating variables to english, improving naming and reorganizing file to make strings easily editable

--- lib/utils/styles/string.dart
+++ lib/utils/styles/string.dart
@@ -1,106 +1,116 @@
 class CustomString {
-  // General
+  // Punctuation
   static const emptyString = "";
-  static const ok = "OK";
+  static const space = " ";
+  static const mark = ".";
+  static const exclamationMark = "!";
+  static const interrogationMark = "?";
+
+  // General
+  static const ok = "ok";
   static const cancel = "Annuler";
   static const success = "Succès";
   static const leave = "Quitter";
-  static const interrogationMark = "?";
-  static const space = " ";
-  static const cfqCapital = "CFQ";
-  static const turnCapital = "TURN";
+  static const orCapital = "OU";
 
   // Authentication
-  static const connexionCapital = "CONNEXION";
-  static const inscriptionCapital = "INSCRIPTION";
-  static const jeMeConnecte = "Je me connecte";
-  static const jemInscris = "Je m'inscris";
-  static const dejaInscrit = "Déjà inscrit(e) ?";
-  static const tAsPasEncoreDeCompte = "T'as pas encore de compte ?";
-  static const tAsOublieTonMotDePasse = "T'as oublié ton mot de passe ?";
-  static const logOut = "Log out";
+  static const logInCapital = "CONNEXION";
+  static const signUpCapital = "INSCRIPTION";
+  static const logIn = "Je me connecte";
+  static const signUp = "Je m'inscris";
+  static const alreadySignedUp = "Déjà inscrit(e) ?";
+  static const noAccountYet = "T'as pas encore de compte ?";
+  static const forgotPassword = "T'as oublié ton mot de passe ?";
+  static const logOut = "Déconnexion";
+
+  // Authentication Form
+  static const yourEmail = "Ton email";
+  static const yourPassword = "Ton mot de passe";
+  static const yourUsername = "Comment on t'appelle ?";
+  static const yourLocation = "T'y es vers où le sang ?";
+  static const yourBirthdate = "Ta date de naissance";
 
   // User Information
-  static const username = "Username";
-  static const unPetitNom = "Un petit nom ?";
-  static const taLocalisation = "Ta localisation";
-  static const taDateDeNaissance = "Ta date de naissance";
-  static const tonMail = "Ton mail";
-  static const tonMotDePasse = "Ton mot de passe";
-  static const tonMood = "Ton mood";
-  static const aucuneLocalisation = "Aucune localisation";
+  static const username = "Nom d'utilisateur";
+  static const noLocation = "Aucune localisation";
 
-  // Search and Friends
+  // Search
   static const search = "Chercher..";
   static const searchUsers = "Chercher des utilisateurs..";
-  static const searchFriends = "Chercher des amis...";
-  static const chercherDesUtilisateurs = "Chercher des utilisateurs...";
-  static const noUsersFound = "No users found.";
-  static const mesAmis = "Mes amis";
-  static const vousnAvezPasEncoredAmis = "Vous n'avez pas encore d'amis";
-  static const amiAjoute = "Ami ajouté !";
-  static const amiSupprime = "Ami supprimé !";
-  static const ajouter = "Ajouter";
-  static const retirer = "Retirer";
+  static const searchFriends = "Chercher des amis..";
+  static const noUsersFound = "Aucun utilisateur ne correspond à ta recherche.";
+  static const userNotFound = "Utilisateur non trouvé";
+
+  // Friends
+  static const myFriends = "Mes amis";
+  static const noFriendsYet = "Vous n'avez pas encore d'amis";
+  static const friendAdded = "Ami ajouté !";
+  static const friendDeleted = "Ami supprimé !";
+  static const addFriend = "Ajouter";
+  static const removeFriend = "Retirer";
 
   // Teams
-  static const ajouterDesMembres = "Ajouter des membres";
-  static const membres = "Membres";
-  static const vousNAvezPasEncoreDeTeam = "Vous n'avez pas encore de team.";
-  static const vousAvezAjouteDesMembres = "Vous avez ajouté des membres !";
-  static const creerUneTeam = "Créer une team";
-  static const noTeamsYet = "Vous n'avez pas encore de teams.";
-  static const mesTeams = "Mes teams";
   static const team = "Team";
+  static const mesTeams = "Mes teams";
+  static const noTeamsYet = "Vous n'avez pas encore de teams.";
+  static const createTeam = "Créer une team";
+  static const teamName = "Nom de la team";
+  static const members = "Membres";
+  static const addMembers = "Ajouter des membres";
   static const leaveTeam = "Quitter l'équipe";
   static const sureToLeaveTeam =
       "Êtes-vous sûr de vouloir quitter cette team ?";
-  static const teamName = "Nom de la team";
-  static const createTeam = "Créer une team";
-  static const members = "Membres";
 
-  // Events and Turns
-  static const creerUnTurn = "Créer un Turn";
-  static const nomDuTurn = "Nom du Turn";
-  static const turn = "Turn";
-  static const caTurn = "Ça turn";
-  static const creerUnCfq = "Créer un CFQ";
-  static const nomDuCfq = "Nom du CFQ";
-  static const cfq = "CFQ";
-  static const caFoutQuoi = "Ça fout quoi ?";
-  static const ajouterUneDate = "Ajouter une date";
-  static const laDate = "La date";
-  static const uneHeure = "1h";
-  static const ou = "Où ?";
-  static const ouCapital = "OU";
-  static const adresse = "Adresse";
-  static const description = "Description";
-  static const moods = "Mood(s)";
-  static const selectionnerLesMoodsDeVotreTurn =
-      "Sélectionnez le(s) mood(s) de votre turn";
-  static const selectionnerLesMoodsDeVotreCfq =
-      "Sélectionnez le(s) mood(s) de votre CFQ";
-  static const organisateurs = "Organisateurs";
-  static const aQui = "À qui ?";
-  static const publier = "Publier";
-  static const publicationReussie = "Publication réussie !";
+  // Events forms
+  static const eventTitle = "Nom de l'évènement";
+  static const organizedBy = "Organisé par";
+  static const organizers = "Organisateurs";
+  static const when = "Quand ?";
+  static const date = "La date";
   static const where = "Où ? (un lieu, un nom, mets ce que tu veux)";
-  static const toutLeMonde = "Tout le monde";
-  static const jeSuisLa = "Je suis là";
   static const address = "Adresse";
+  static const whatMood = "Quel mood ?";
   static const describeEvent =
       "Raconte pas ta vie, dis-nous juste où tu sors...";
-  static const organizedBy = "Organisé par";
-  static const eventTitle = "Titre de l'event";
-  static const when = "Quand ?";
-  static const whatMood = "Quel mood ?";
+
+  static const invitees = "T'invites qui ?";
+  static const toutLeMonde = "Tout le monde";
+
+  static const publier = "Publier";
+  static const publicationReussie = "Publication réussie !";
+
+  static const jeSuisLa = "Je suis là";
   static const going = "y vont";
 
+  // Turns
+  static const turnCapital = "TURN";
+  static const createTurn = "Créer un Turn";
+  static const turnName = "Nom du Turn";
+  static const caTurn = "Ça turn";
+
+  // CFQs
+  static const cfqCapital = "CFQ";
+  static const createCfq = "Créer un CFQ";
+  static const cfqName = "Nom du CFQ";
+  static const caFoutQuoi = "Ça fout quoi ?";
+
   // Feeds
-  static const sesPosts = "Ses posts";
-  static const mesPosts = "Mes posts";
-  static const sonCalendrier = "Son calendrier";
-  static const monCalendrier = "Mon calendrier";
+  static const myPosts = "Mes posts";
+  static const otherUserPosts = "Ses posts";
+  static const myCalendar = "Mon calendrier";
+  static const otherUserCalendar = "Son calendrier";
+
+  // Parameters
+  static const parameters = "Paramètres";
+  static const editProfile = "Editer le profil";
+  static const notifications = "Notifications";
+  static const privacy = "Confidentialité";
+
+  // Image Related
+  static const noImage = "Aucune image";
+  static const pickImageFromGallery = "Choisir une photo de la galerie";
+  static const takePictureWithDevice = "Prendre une photo avec l'appareil";
+  static const pleaseSelectAnImage = "Veuillez sélectionner une image";
 
   // Moods
   static const houseMood = "maison";
@@ -110,34 +120,23 @@ class CustomString {
   static const turnMood = "turn";
   static const chillMood = "chill";
 
-  // Parameters
-  static const parametres = "Paramètres";
-  static const editerLeProfil = "Editer le profil";
-  static const seDeconnecter = "Se déconnecter";
-  static const notifications = "Notifications";
-  static const confidentialite = "Confidentialité";
-
   // Error Messages
-  static const errorFetchingEvents = "Error fetching events";
-  static const failedtoUpdateStatusPleaseTryAgain =
-      "Failed to update status. Please try again.";
+  static const errorFetchingEvents =
+      "Erreur lors de la récupération des évènements";
+  static const failedToUpdateStatusPleaseTryAgain =
+      "Erreur lors de la mise à jour du statut. Veuillez réessayer.";
   static const failedToUploadProfilePicture =
-      "Failed to upload profile picture";
-  static const someErrorOccurred = "Some error occurred";
+      "Erreur lors de l'upload de la photo de profil. Veuillez réessayer.";
+  static const someErrorOccurred = "Une erreur s'est produite";
   static const veuillezRemplirTousLesChamps =
       "Veuillez remplir tous les champs";
   static const pleaseFillInAllRequiredFields =
-      "Please fill in all required fields";
-  static const veuillezSelectionnerUneImage = "Veuillez sélectionner une image";
-  static const thisIsWeb = "This is web";
-  static const fetchingDataNoEventsYet = "Fetching data, no events yet...";
-  static const noEventsAvailable = "No events available";
-  static const userNotFound = "Utilisateur non trouvé";
+      "Veuillez remplir tous les champs requis";
+  static const fetchingDataNoEventsYet =
+      "Récupération des données, pas d'évènements pour le moment...";
+  static const noEventsAvailable = "Aucun évènement disponible";
   static const errorLeavingTeam = "Erreur lors de la sortie de la team";
 
-  // Image Related
-  static const aucuneImage = "Aucune image";
-  static const choisirUnePhotoDeLaGalerie = "Choisir une photo de la galerie";
-  static const prendreUnePhotoAveclAppareil =
-      "Prendre une photo avec l'appareil";
+  // Utils
+  static const thisIsWeb = "C'est web";
 }
