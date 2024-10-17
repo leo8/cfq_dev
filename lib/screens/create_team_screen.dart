9ff3858bda12b60f4f658a95ad3e67b19ede2238
leo8 <leoleo8@hotmail.fr>
2024-10-18 00:12:17 +0200

    Fix overflow issue

--- lib/screens/create_team_screen.dart
+++ lib/screens/create_team_screen.dart
@@ -47,192 +47,186 @@ class CreateTeamScreen extends StatelessWidget {
                 }
               });
 
-              return Stack(
+              return ListView(
+                padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom,
+                ),
                 children: [
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 15),
+                        Text(
+                          'NOUVELLE TEAM',
+                          style: CustomTextStyle.title3.copyWith(fontSize: 28),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 20),
+                        Stack(
                           children: [
-                            const SizedBox(height: 15),
-                            Text(
-                              'NOUVELLE TEAM',
-                              style:
-                                  CustomTextStyle.title3.copyWith(fontSize: 28),
-                              textAlign: TextAlign.center,
+                            CircleAvatar(
+                              radius: 100,
+                              backgroundColor: viewModel.teamImage != null
+                                  ? null
+                                  : CustomColor.customBlack,
+                              backgroundImage: viewModel.teamImage != null
+                                  ? MemoryImage(viewModel.teamImage!)
+                                  : null,
+                              child: viewModel.teamImage == null
+                                  ? Container(
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(
+                                          color: CustomColor.customWhite,
+                                          width: 0.5,
+                                        ),
+                                      ),
+                                    )
+                                  : null,
                             ),
-                            const SizedBox(height: 30),
-                            Stack(
-                              children: [
-                                CircleAvatar(
-                                  radius: 100,
-                                  backgroundColor: viewModel.teamImage != null
-                                      ? null
-                                      : CustomColor.customBlack,
-                                  backgroundImage: viewModel.teamImage != null
-                                      ? MemoryImage(viewModel.teamImage!)
-                                      : null,
-                                  child: viewModel.teamImage == null
-                                      ? Container(
-                                          decoration: BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            border: Border.all(
-                                              color: CustomColor.customWhite,
-                                              width: 0.5,
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Center(
+                                child: IconButton(
+                                  icon: CustomIcon.addImage,
+                                  onPressed: viewModel.pickTeamImage,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        BorderedIconTextField(
+                          icon: CustomIcon.favorite,
+                          controller: viewModel.teamNameController,
+                          hintText: CustomString.teamName,
+                        ),
+                        const SizedBox(height: 10),
+                        GestureDetector(
+                          onTap: () => viewModel
+                              .performSearch(viewModel.searchController.text),
+                          child: CustomSearchBar(
+                            controller: viewModel.searchController,
+                            hintText: CustomString.addFriends,
+                            onChanged: (value) =>
+                                viewModel.performSearch(value),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 130, // Adjust this height as needed
+                          child: SingleChildScrollView(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: Column(
+                                children: [
+                                  if (viewModel.isSearching) ...[
+                                    const CircularProgressIndicator(),
+                                  ] else if (viewModel
+                                      .searchResults.isNotEmpty) ...[
+                                    ListView.builder(
+                                      shrinkWrap: true,
+                                      physics:
+                                          const NeverScrollableScrollPhysics(),
+                                      itemCount: viewModel.searchResults.length,
+                                      itemBuilder: (context, index) {
+                                        final user =
+                                            viewModel.searchResults[index];
+                                        return ListTile(
+                                          leading: Container(
+                                            decoration: BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              boxShadow: user.isActive
+                                                  ? [
+                                                      const BoxShadow(
+                                                        color: CustomColor
+                                                            .turnColor,
+                                                        blurRadius: 5,
+                                                        spreadRadius: 1,
+                                                      ),
+                                                    ]
+                                                  : null,
+                                            ),
+                                            child: CircleAvatar(
+                                              backgroundImage: NetworkImage(
+                                                  user.profilePictureUrl),
                                             ),
                                           ),
-                                        )
-                                      : null,
-                                ),
-                                Positioned(
-                                  bottom: 0,
-                                  left: 0,
-                                  right: 0,
-                                  child: Center(
-                                    child: IconButton(
-                                      icon: CustomIcon.addImage,
-                                      onPressed: viewModel.pickTeamImage,
+                                          title: Text(user.username),
+                                          trailing: IconButton(
+                                            icon: CustomIcon.add,
+                                            onPressed: () =>
+                                                viewModel.addFriend(user),
+                                          ),
+                                        );
+                                      },
                                     ),
+                                  ] else if (!viewModel.isSearching &&
+                                      viewModel.searchController.text
+                                          .isNotEmpty) ...[
+                                    Center(
+                                      child: Text(
+                                        CustomString.noResults,
+                                        style: CustomTextStyle.body2,
+                                      ),
+                                    ),
+                                  ],
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 90, // Adjust this height as needed
+                          padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                if (viewModel.selectedFriends.isNotEmpty)
+                                  Wrap(
+                                    spacing: 8.0,
+                                    runSpacing: 4.0,
+                                    children:
+                                        viewModel.selectedFriends.map((friend) {
+                                      bool isCurrentUser = friend.uid ==
+                                          viewModel.currentUser?.uid;
+                                      return Chip(
+                                        avatar: CircleAvatar(
+                                          backgroundImage: NetworkImage(
+                                              friend.profilePictureUrl),
+                                        ),
+                                        label: Text(friend.username),
+                                        onDeleted: isCurrentUser
+                                            ? null
+                                            : () =>
+                                                viewModel.removeFriend(friend),
+                                      );
+                                    }).toList(),
                                   ),
-                                ),
                               ],
                             ),
-                            const SizedBox(height: 30),
-                            BorderedIconTextField(
-                              icon: CustomIcon.favorite,
-                              controller: viewModel.teamNameController,
-                              hintText: CustomString.teamName,
-                            ),
-                            const SizedBox(height: 20),
-                            GestureDetector(
-                              onTap: () => viewModel.performSearch(
-                                  viewModel.searchController.text),
-                              child: CustomSearchBar(
-                                controller: viewModel.searchController,
-                                hintText: CustomString.addFriends,
-                                onChanged: (value) =>
-                                    viewModel.performSearch(value),
-                              ),
-                            ),
-                          ],
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 200, // Adjust this height as needed
-                              child: SingleChildScrollView(
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 16.0),
-                                  child: Column(
-                                    children: [
-                                      if (viewModel.isSearching) ...[
-                                        const CircularProgressIndicator(),
-                                      ] else if (viewModel
-                                          .searchResults.isNotEmpty) ...[
-                                        ListView.builder(
-                                          shrinkWrap: true,
-                                          physics:
-                                              const NeverScrollableScrollPhysics(),
-                                          itemCount:
-                                              viewModel.searchResults.length,
-                                          itemBuilder: (context, index) {
-                                            final user =
-                                                viewModel.searchResults[index];
-                                            return ListTile(
-                                              leading: Container(
-                                                decoration: BoxDecoration(
-                                                  shape: BoxShape.circle,
-                                                  boxShadow: user.isActive
-                                                      ? [
-                                                          const BoxShadow(
-                                                            color: CustomColor
-                                                                .turnColor,
-                                                            blurRadius: 5,
-                                                            spreadRadius: 1,
-                                                          ),
-                                                        ]
-                                                      : null,
-                                                ),
-                                                child: CircleAvatar(
-                                                  backgroundImage: NetworkImage(
-                                                      user.profilePictureUrl),
-                                                ),
-                                              ),
-                                              title: Text(user.username),
-                                              trailing: IconButton(
-                                                icon: CustomIcon.add,
-                                                onPressed: () =>
-                                                    viewModel.addFriend(user),
-                                              ),
-                                            );
-                                          },
-                                        ),
-                                      ] else if (!viewModel.isSearching &&
-                                          viewModel.searchController.text
-                                              .isNotEmpty) ...[
-                                        Center(
-                                          child: Text(
-                                            CustomString.noResults,
-                                            style: CustomTextStyle.body2,
-                                          ),
-                                        ),
-                                      ],
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Expanded(
-                              flex: 1,
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 16.0),
-                                child: SingleChildScrollView(
-                                  child: Column(
-                                    children: [
-                                      if (viewModel.selectedFriends.isNotEmpty)
-                                        Wrap(
-                                          spacing: 8.0,
-                                          runSpacing: 4.0,
-                                          children: viewModel.selectedFriends
-                                              .map((friend) {
-                                            bool isCurrentUser = friend.uid ==
-                                                viewModel.currentUser?.uid;
-                                            return Chip(
-                                              avatar: CircleAvatar(
-                                                backgroundImage: NetworkImage(
-                                                    friend.profilePictureUrl),
-                                              ),
-                                              label: Text(friend.username),
-                                              onDeleted: isCurrentUser
-                                                  ? null
-                                                  : () => viewModel
-                                                      .removeFriend(friend),
-                                            );
-                                          }).toList(),
-                                        ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: CustomButton(
-                          label: CustomString.create,
-                          onTap: viewModel.createTeam,
-                          isLoading: viewModel.isLoading,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: CustomButton(
+                      label: CustomString.create,
+                      onTap: viewModel.createTeam,
+                      isLoading: viewModel.isLoading,
+                    ),
                   ),
                 ],
               );
