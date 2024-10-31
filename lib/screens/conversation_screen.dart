f30fa06e2e51ce25b5495f2c67936d86ec49058e
leo8 <leoleo8@hotmail.fr>
2024-10-31 14:42:54 +0100

    Working on option buttons UI

--- lib/screens/conversation_screen.dart
+++ lib/screens/conversation_screen.dart
@@ -121,13 +121,11 @@ class _ConversationScreenState extends State<ConversationScreen> {
               children: [
                 Row(
                   children: [
-                    Expanded(
-                      child: Text(
-                        widget.eventName,
-                        style: CustomTextStyle.title1,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                      ),
+                    Text(
+                      widget.eventName,
+                      style: CustomTextStyle.title1,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
                     ),
                     const SizedBox(width: 12),
                     IconButton(
@@ -165,7 +163,10 @@ class _ConversationScreenState extends State<ConversationScreen> {
         return Container(
           child: Wrap(
             children: <Widget>[
+              const Divider(height: 20, color: CustomColor.transparent),
+              const Divider(),
               ListTile(
+                minTileHeight: 45,
                 leading:
                     Icon(_isInUserConversations ? Icons.remove : Icons.add),
                 title: Text(_isInUserConversations
@@ -184,13 +185,19 @@ class _ConversationScreenState extends State<ConversationScreen> {
                   Navigator.pop(context);
                 },
               ),
+              const Divider(),
               ListTile(
+                minTileHeight: 45,
                 leading: const Icon(Icons.people),
                 title: const Text(CustomString.seeMembers),
                 onTap: () {
                   Navigator.pop(context);
                   _showInviteesList(context);
                 },
+              ),
+              const Divider(),
+              const SizedBox(
+                height: 120,
               ),
             ],
           ),
