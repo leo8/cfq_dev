7ce0760fb73cc0f6f10dacc5d7c9fdb445cc28a2
leo8 <leoleo8@hotmail.fr>
2024-11-06 18:20:30 +0100

    Ensure consistent style of notification card for team request

--- lib/widgets/molecules/notification_card.dart
+++ lib/widgets/molecules/notification_card.dart
@@ -49,47 +49,20 @@ class NotificationCard extends StatelessWidget {
       case model.NotificationType.teamRequest:
         final content =
             notification.content as model.TeamRequestNotificationContent;
-        return InkWell(
-          onTap: onTap,
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    CustomAvatar(
-                      imageUrl: content.inviterProfilePictureUrl,
-                      radius: 25,
-                    ),
-                    const SizedBox(width: 12),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                              style: CustomTextStyle.body1,
-                              children: [
-                                TextSpan(
-                                  text: content.inviterUsername,
-                                  style: CustomTextStyle.body1Bold,
-                                ),
-                                const TextSpan(text: ' t\'invite à rejoindre '),
-                                TextSpan(
-                                  text: content.teamName,
-                                  style: CustomTextStyle.body1Bold,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+        return _buildNotificationRow(
+          avatarUrl: content.inviterProfilePictureUrl,
+          content: [
+            TextSpan(
+              text: content.inviterUsername,
+              style: CustomTextStyle.body1Bold,
             ),
-          ),
+            const TextSpan(text: ' t\'invite à rejoindre '),
+            TextSpan(
+              text: content.teamName,
+              style: CustomTextStyle.body1Bold,
+            ),
+          ],
+          timestamp: notification.timestamp,
         );
 
       case model.NotificationType.followUp:
