b972d97b13dc84916758dcf9e5ceaf29d620de65
CalvignacNeoxia <charles.calvignac@neoxia.com>
2024-11-13 11:19:14 +0100

    edit secrets

--- lib/secrets/secrets_firebase.dart
+++ lib/secrets/secrets_firebase.dart
@@ -1,5 +1,5 @@
 var apiKey = "AIzaSyA-8CvTsGTXuzocPMmj7LvUs-6fsQ_NNPU"; // Your API key
-var appId = "1:451535553993:ios:c5a658646f42726d095418"; // Your app ID
+var appId = "1:451535553993:web:f0432e8487dee385095418"; // Your app ID
 var messagingSenderId = "451535553993"; // Sender ID
 var projectId = "cfq-prod-477ed"; // Project ID
-var storageBucket = "cfq-prod-477ed.appspot.com";
+var storageBucket = "cfq-prod-477ed.firebasestorage.app";
